ee428a016b908c51a8ee9af06e993d4e386aa087
Bhavya Goel <bgoel4132@gmail.com>
2021-05-28 04:00:53 +0530

    speech-sign and database show

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -1,23 +1,33 @@
+import re
 import streamlit as st
 import queue
-from streamlit.elements import form
 import av
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
-import pyrebase
 from streamlit_webrtc import (
     webrtc_streamer, 
     VideoProcessorBase,
     WebRtcMode,
+    ClientSettings
 )
 from typing import List, NamedTuple
 from PIL import Image, ImageOps
 import cv2
 import tensorflow.keras
 import numpy as np
-np.set_printoptions(suppress=True)
+import pandas as pd
+from bokeh.models.widgets import Button
+from bokeh.models import CustomJS
+from streamlit_bokeh_events import streamlit_bokeh_events
 
+np.set_printoptions(suppress=True)
+WEBRTC_CLIENT_SETTINGS = ClientSettings(
+    rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
+    media_stream_constraints={"video": True, "audio": True},
+)
+
+global query_param 
 
 def firebase():
     if not firebase_admin._apps:
@@ -30,13 +40,9 @@ def firebase():
 @st.cache
 def cache_query_param():
     query_param = st.experimental_get_query_params()
-    return query_param
-
-def read_user_info(db, query_param):    
-    doc_ref = db.collection(u'users').document(query_param['user'][0])
-    doc = doc_ref.get()
-    if doc.exists:
-        return doc.to_dict()
+    print(query_param)
+    user_id = query_param['user'][0]
+    return user_id, query_param
 
 def gen_labels():
         labels = {}
@@ -59,7 +65,8 @@ class VideoTransformer(VideoProcessorBase):
     def __init__(self) -> None:
         self.threshold1 = 224
         self.result_queue = queue.Queue()
-
+        self.data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+    
     def _predict_image(self, image, model):
         result: List[Detection] = []
         labels = gen_labels()
@@ -72,28 +79,27 @@ class VideoTransformer(VideoProcessorBase):
         return result
 
     def recv(self, frame: av.VideoFrame) -> av.VideoFrame:
+        self.frame = frame
         img = frame.to_ndarray(format="bgr24")
-        frame = cv2.resize(img, (224,224))
-        frame = Image.fromarray(frame)
-        data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+        frm = cv2.resize(img, (224,224))
+        frm = Image.fromarray(frm)
         size = (224, 224)
-        image = ImageOps.fit(frame, size, Image.ANTIALIAS)
+        image = ImageOps.fit(frm, size, Image.ANTIALIAS)
         image_array = np.asarray(image)
         normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
-        data[0] = normalized_image_array
-        model = tensorflow.keras.models.load_model("model/keras_model.h5")
-        
-        result = self._predict_image(data, model)
-        
+        self.data[0] = normalized_image_array
+        model = tensorflow.keras.models.load_model("model/keras_model.h5", compile=False)
+        result = self._predict_image(self.data, model)
         self.result_queue.put(result)
 
-        return av.VideoFrame.from_ndarray(img, format="bgr24")
+        return 0
 
-def sign_detection():
+def sign_detection(db, user_id):
     
     ctx = webrtc_streamer(
         key="SpeechSign",
         mode=WebRtcMode.SENDRECV,
+        client_settings=WEBRTC_CLIENT_SETTINGS,
         video_processor_factory=VideoTransformer, 
         async_processing=True,)
     
@@ -106,11 +112,72 @@ def sign_detection():
                         result = ctx.video_processor.result_queue.get(
                             timeout=1.0
                         )
+                        doc_ref = db.collection(u'users').document(user_id).collection('sign-detected').add({result[0].name:result[0].prob})
                     except queue.Empty:
                         result = None
+                    
                     labels_placeholder.table(result)
                 else:
                     break
+
+def speech_detection():
+    st.header("Press the following button to speak.")
+    st.markdown("As soon as you press the button, microphone of your device gets activated and your audio is converted to Sign Language.")
+    stt_button = Button(label="Speak", width=100)
+
+    stt_button.js_on_event("button_click", CustomJS(code="""
+        var recognition = new webkitSpeechRecognition();
+        recognition.continuous = true;
+        recognition.interimResults = true;
+    
+        recognition.onresult = function (e) {
+            var value = "";
+            for (var i = e.resultIndex; i < e.results.length; ++i) {
+                if (e.results[i].isFinal) {
+                    value += e.results[i][0].transcript;
+                }
+            }
+            if ( value != "") {
+                document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
+            }
+        }
+        recognition.start();
+        """))
+
+    result = streamlit_bokeh_events(
+        stt_button,
+        events="GET_TEXT",
+        key="listen",
+        refresh_on_update=False,
+        override_height=75,
+        debounce_time=0)
+
+    if result:
+        if "GET_TEXT" in result:
+            text = result.get("GET_TEXT")
+            text = text.upper()
+            for i in text:
+                st.image("static/sign_alpha/"+i+".jpg", width=200, caption=i, use_column_width=True)
+    return 0
+
+def show_database(db, user_id):
+    doc_ref = db.collection(u'users').document(user_id)
+    doc = doc_ref.get()
+    user_det = doc.to_dict()
+    st.header("User Details")
+    user_df = pd.DataFrame({"Type":['Name','DOB','Email'], "Value":[user_det['name'],user_det['dob'],user_det['email']]})
+    st.dataframe(user_df)
+
+    st.header('Sign Detected')
+    sign_df = pd.DataFrame(columns=['Alphabet','Confidence'])
+    sign_ref = doc_ref.collection(u'sign-detected').stream()
+    for sign in sign_ref:
+        res = sign.to_dict()
+        for alphabet,prob in res.items():
+            sign_df = sign_df.append({'Alphabet':alphabet,'Confidence':prob}, ignore_index=True)
+
+    st.dataframe(sign_df)
+
 
 def main():
     image = Image.open("static/vid_call.jpg")
@@ -118,10 +185,23 @@ def main():
     st.set_page_config(page_title="SpeechSign", page_icon=logo)
     st.image(image)
     st.title("@SpeechSign")
-    app, db = firebase()
-    query_param = cache_query_param()
-    user_det = read_user_info(db, query_param)
-    sign_detection()
+    user_id, query_param = cache_query_param()
 
+    app, db = firebase()
+    st.sidebar.title("Select the process to your convinience")
+    st.sidebar.markdown("Select the conversion method accordingly:")
+    algo = st.sidebar.selectbox(
+        "Select the Operation", options=["Sign-to-Speech", "Speech-to-Sign", "Access Database"]
+    )
+
+    if algo == "Sign-to-Speech":
+        sign_detection(db, user_id=user_id)
+    elif algo == "Speech-to-Sign":
+        speech_detection()
+    elif algo == "Access Database":
+        show_database(db, user_id=user_id)
+ 
+    
 if __name__ == "__main__":
+    
     main()
